f3e789032177ad3b137c0aa0a8102254d56aa5d6
ishtiakanam <iafahim68@gmail.com>
2021-09-23 16:58:35 +0600

    website-updated

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,14 +6,19 @@ const Card = (props) => {
     for(const product of card){
         total = total + product.price;
     }
+      const shipping = 15;
+      const tax = (total + shipping)* 10;
+      const grandTotal = total + shipping + tax;
 
-    
     return (
         <div>
             <h3>Order summery</h3>
             <h5>Item Ordered:{props.card.length}</h5>
             <br />
             <p> Total:{total}</p>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand total:{grandTotal.toFixed(2)}</p>
         </div>
     );
 };
